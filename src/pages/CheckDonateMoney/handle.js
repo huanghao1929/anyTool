955267ce291a87c86d862ebfbb54a946e5702139
黄浩 <huanghao@>
2024-08-17 19:14:44 +0800

    一次性打印全部异常数据

--- src/pages/CheckDonateMoney/handle.js
+++ src/pages/CheckDonateMoney/handle.js
@@ -1,4 +1,4 @@
-import {getArrayByXlsxFile} from '@/utils/xlsxUtil';
+import { getArrayByXlsxFile } from "@/utils/xlsxUtil";
 
 // 将扁平化的数组按照文件夹转换成树状结构
 export const buildFileTree = (flatFiles) => {
@@ -46,12 +46,12 @@ const getSummaryInfoByXlsx = async (xlsxFile) => {
   const fileData = await xlsxFile.async("blob");
   const fileArray = await getArrayByXlsxFile(fileData);
   // 二维数组中，移除首行的信息，
-  const removeFitst = fileArray.toSpliced(0,1);
+  const removeFitst = fileArray.toSpliced(0, 1);
 
- //  并且从后往前，移除空的信息
-  for (let index = removeFitst.length - 1; index >= 0; index --) {
+  //  并且从后往前，移除空的信息
+  for (let index = removeFitst.length - 1; index >= 0; index--) {
     const element = removeFitst[index];
-    if(element.length === 0) {
+    if (element.length === 0) {
       removeFitst.pop();
     } else {
       break;
@@ -74,8 +74,8 @@ export const checkTreeZip = async (tree) => {
   const errorList = [];
 
   // 递归的函数 参数除了树本身，加入 path 来方便排查问题
-  const checkOneTree = async (tree, path = "/") => {
-    console.log('开始检查当前path', path, tree);
+  const checkOneTree = async (tree, path = "根文件") => {
+    // console.log("开始检查当前path", path, tree);
     // 1. 检查当前数组中，是否有汇总表结尾的 xlsx 文件
     const summaryXlsx = tree.find((item) => /汇总表.xlsx$/.test(item.name));
     // 2. 当前文件夹的数组，方便后面访问
@@ -88,67 +88,72 @@ export const checkTreeZip = async (tree) => {
           msg: "当前没有汇总表但是有文件夹",
         };
         errorList.push(errInfo);
+      } else {
+        // 没有汇总表，也没有文件夹，直接结束当前递归。
         return;
       }
-      // 没有汇总表，也没有文件夹，直接结束当前递归。
-      return;
     }
 
-    // 4. 获取到当前汇总表的信息
-    const summaryInfo = await getSummaryInfoByXlsx(summaryXlsx.file);
-    // 5. 检查汇总表信息之前，需要保证汇总表每条信息的准确性
-    for (let index = 0; index < summaryInfo.length; index++) {
-      const element = summaryInfo[index];
-      const [promoter] = element;
-      if(!promoter) {
+    // 下面几个检查，都是有汇总表才行
+    if (summaryXlsx) {
+      // 4. 获取到当前汇总表的信息
+      const summaryInfo = await getSummaryInfoByXlsx(summaryXlsx.file);
+      // 5. 检查汇总表信息之前，需要保证汇总表每条信息的准确性
+      for (let index = 0; index < summaryInfo.length; index++) {
+        const element = summaryInfo[index];
+        const [promoter] = element;
+        if (!promoter) {
+          const errInfo = {
+            path,
+            msg: `当前路径下的汇总表这一条信息没有发起人-${element}`,
+          };
+          errorList.push(errInfo);
+          // return;
+        }
+        if (promoter.length > 30) {
+          const errInfo = {
+            path,
+            msg: `当前路径下的汇总表这一条信息发起人长度大于30-${element}`,
+          };
+          errorList.push(errInfo);
+          // return;
+        }
+      }
+
+      // 6. 汇总表信息条数需要严格等于文件夹的个数
+      if (summaryInfo.length !== dirArray.length) {
         const errInfo = {
           path,
-          msg: `当前路径下的汇总表这一条信息没有发起人-${element}`,
+          msg: "当前路径下汇总表信息数和文件夹的个数不一致",
         };
         errorList.push(errInfo);
-        return;
+        // return;
       }
-      if(promoter.length > 30) {
-        const errInfo = {
-          path,
-          msg: `当前路径下的汇总表这一条信息发起人长度大于30-${element}`,
-        };
-        errorList.push(errInfo);
-        return;
-      }
-    }
 
-    // 6. 汇总表信息条数需要严格等于文件夹的个数
-    if(summaryInfo.length !== dirArray.length) {
-      const errInfo = {
-        path,
-        msg: "当前路径下汇总表信息数和文件夹的个数不一致",
-      };
-      errorList.push(errInfo);
-      return;
-    }
-
-    // 7. 检查两个汇总表与文件夹的名称是否完全相同，此时这俩长度相同。那么for循环一个就可以了。
-    // 以汇总表为准。
-    for (let index = 0; index < summaryInfo.length; index++) {
-      const element = summaryInfo[index];
-      const [promoter] = element;
-      const isHasDir = dirArray.some((item) => item.name === promoter);
-      if(!isHasDir) {
-        const errInfo = {
-          path,
-          msg: `当前路径下${promoter}在汇总表，但是没有对应的文件夹`,
-        };
-        errorList.push(errInfo);
-        return;
+      // 7. 检查两个汇总表与文件夹的名称是否完全相同，此时这俩长度相同。那么for循环一个就可以了。
+      // 以汇总表为准。
+      for (let index = 0; index < summaryInfo.length; index++) {
+        const element = summaryInfo[index];
+        const [promoter] = element;
+        const isHasDir = dirArray.some((item) => item.name === promoter);
+        if (!isHasDir) {
+          const errInfo = {
+            path,
+            msg: `当前路径下${promoter}在汇总表，但是没有对应的文件夹`,
+          };
+          errorList.push(errInfo);
+          // return;
+        }
       }
     }
 
-
-    // 上述检查没有问题之后，可以进入到下一级的文件夹
+    console.log(path, "准备遍历当前文件夹", dirArray);
+    // 上述检查之后，可以进入到下一级的文件夹
     for (let index = 0; index < dirArray.length; index++) {
       const element = dirArray[index];
-      checkOneTree(element.children, `${path}/${element.name}`)
+      // console.log("开始检查", `${path}/${element.name}`, index);
+      await checkOneTree(element.children, `${path}/${element.name}`);
+      // console.log("结束检查", `${path}/${element.name}`);
     }
   };
 
